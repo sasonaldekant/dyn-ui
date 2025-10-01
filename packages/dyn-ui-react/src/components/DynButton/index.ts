beaff7476a75225b1aa45463f22bfdcf7c5f807d
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-01 18:00:55 +0200

    Update DynButton index exports to match SCOPE 3 specification

---
+++ packages/dyn-ui-react/src/components/DynButton/index.ts
@@ -0,0 +1,4 @@
+// DynButton component exports according to original SCOPE 3 specification
+export { DynButton } from './DynButton';
+export { default } from './DynButton';
+export type { DynButtonProps } from './DynButton.types';
