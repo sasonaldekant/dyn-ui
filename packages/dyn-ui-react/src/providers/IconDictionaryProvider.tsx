55b939a7bcaa55521cc54e36a0f8541f93de7b46
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 02:01:50 +0200

    Add IconDictionaryProvider context for Icon system - SCOPE 5

---
+++ packages/dyn-ui-react/src/providers/IconDictionaryProvider.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { IconDictionary } from '../types/icon.types';
+
+interface IconDictionaryContextType {
+  [key: string]: string;
+}
+
+export const IconDictionaryContext = createContext<IconDictionaryContextType>({});
+
+interface Props {
+  children: React.ReactNode;
+  customDictionary?: IconDictionary;
+}
+
+export const IconDictionaryProvider: React.FC<Props> = ({ children, customDictionary }) => {
+  const [dictionary, setDictionary] = useState<IconDictionaryContextType>({});
+
+  useEffect(() => {
+    // Load default DYN UI icons + custom dictionary
+    const defaultIcons: IconDictionary = {
+      'user': 'dyn-icon-user',
+      'home': 'dyn-icon-home',
+      'settings': 'dyn-icon-settings',
+      'ok': 'dyn-icon-ok',
+      'close': 'dyn-icon-close',
+      'warning': 'dyn-icon-warning',
+      'minus': 'dyn-icon-minus',
+      'plus': 'dyn-icon-plus',
+      'search': 'dyn-icon-search',
+      'edit': 'dyn-icon-edit',
+      'delete': 'dyn-icon-delete',
+      'arrow-up': 'dyn-icon-arrow-up',
+      'arrow-down': 'dyn-icon-arrow-down',
+      'arrow-left': 'dyn-icon-arrow-left',
+      'arrow-right': 'dyn-icon-arrow-right',
+      'menu': 'dyn-icon-menu',
+      'info': 'dyn-icon-info',
+      'calendar': 'dyn-icon-calendar',
+      'clock': 'dyn-icon-clock',
+      'mail': 'dyn-icon-mail'
+      // Add more default mappings as needed
+    };
+
+    setDictionary({
+      ...defaultIcons,
+      ...customDictionary
+    });
+  }, [customDictionary]);
+
+  return (
+    <IconDictionaryContext.Provider value={dictionary}>
+      {children}
+    </IconDictionaryContext.Provider>
+  );
+};
