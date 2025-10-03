6765f44db679b4fecc8c3a8a26ae130a17facf7d
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 02:01:14 +0200

    Add Label types for Display Components Group - SCOPE 5

---
+++ packages/dyn-ui-react/src/types/label.types.ts
@@ -0,0 +1,9 @@
+export interface DynLabelProps {
+  children: React.ReactNode;
+  htmlFor?: string;
+  disabled?: boolean;
+  required?: boolean;
+  optional?: boolean;
+  helpText?: string;
+  className?: string;
+}
