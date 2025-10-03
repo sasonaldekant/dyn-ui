692a02ea6fbda599024c157b5a17b610a9ee364e
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 02:18:35 +0200

    Add types index with all Display Components types - SCOPE 5

---
+++ packages/dyn-ui-react/src/types/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Type exports for DYN UI React
+ * Updated to include Display Components types - SCOPE 5
+ */
+
+// Theme types
+export type { ThemeName, ThemeConfig, ThemeContextValue, ColorVariant, Size } from './theme';
+
+// Display Components types - SCOPE 5
+export type { DynBadgeProps, BadgeStatus, BadgeSize, BadgeIcon } from './badge.types';
+export type { DynAvatarProps, AvatarSize, AvatarLoading } from './avatar.types';
+export type { DynIconProps, IconDictionary, ProcessedIcon } from './icon.types';
+export type { DynLabelProps } from './label.types';
+
+// Constants
+export { DYN_COLOR_PALETTE } from './badge.types';
+export { AVATAR_SIZES } from './avatar.types';
