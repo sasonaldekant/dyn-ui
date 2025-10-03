f3e235ce9cfafaf7fec64c0e083b028d7fe40b44
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 02:17:51 +0200

    Add providers index with IconDictionaryProvider export - SCOPE 5

---
+++ packages/dyn-ui-react/src/providers/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Provider exports for DYN UI React
+ * Updated to include IconDictionaryProvider - SCOPE 5
+ */
+
+export { ThemeProvider } from './ThemeProvider';
+export { IconDictionaryProvider, IconDictionaryContext } from './IconDictionaryProvider';
+
+// Export types
+export type { ThemeProviderProps } from './ThemeProvider';
