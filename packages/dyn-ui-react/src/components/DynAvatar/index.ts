b3780441f4c9513244d559d18d7cf01d328db5ed
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 02:05:57 +0200

    Add DynAvatar export index - SCOPE 5

---
+++ packages/dyn-ui-react/src/components/DynAvatar/index.ts
@@ -0,0 +1,3 @@
+export { default } from './DynAvatar';
+export type { DynAvatarProps, AvatarSize, AvatarLoading } from '../../types/avatar.types';
+export { AVATAR_SIZES } from '../../types/avatar.types';
