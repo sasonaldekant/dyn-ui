35221872569184728c493f0220e7d0e2af4fe63e
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 01:54:17 +0200

    Remove ThemeProvider export from providers index

--- packages/dyn-ui-react/src/providers/index.ts
+++ packages/dyn-ui-react/src/providers/index.ts
@@ -1,9 +1,8 @@
-﻿/**
+/**
  * Provider exports for DYN UI React
  * Updated to include IconDictionaryProvider - SCOPE 5
  */
 
-export { ThemeProvider } from './ThemeProvider';
 export { IconDictionaryProvider, IconDictionaryContext } from './IconDictionaryProvider';
 
 // Export types
