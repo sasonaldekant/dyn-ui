7388e02af9574e99524be4fb2e786f30bff3c965
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 19:10:33 +0200

    Update component exports and add theme system

--- packages/dyn-ui-react/src/components/index.ts
+++ packages/dyn-ui-react/src/components/index.ts
@@ -3,17 +3,28 @@
  * Updated to include Display Components Group - SCOPE 5
  */
 
+// Import global theme styles
+import '../styles/themes.css';
+
 // Basic components
 export { DynButton } from './DynButton';
 export { DynIcon } from './DynIcon';
 
 // Display Components - SCOPE 5
-export { default as DynBadge } from './DynBadge';
-export { default as DynAvatar } from './DynAvatar';
-export { default as DynLabel } from './DynLabel';
+export { DynBadge } from './DynBadge';
+export { DynAvatar } from './DynAvatar';
+export { DynLabel } from './DynLabel';
 
 // Layout components
 export { DynContainer } from './DynContainer';
+export { DynDivider } from './DynDivider';
+
+// Utility components
+export { ThemeSwitcher } from './ThemeSwitcher';
+
+// Theme system
+export { ThemeProvider, useTheme } from '../theme/ThemeProvider';
+export type { ThemeProviderProps, ThemeContextValue } from '../theme/ThemeProvider';
 
 // Export types
 export type { DynButtonProps } from './DynButton/DynButton.types';
@@ -24,6 +35,9 @@ export type { DynBadgeProps, BadgeStatus, BadgeSize } from '../types/badge.types
 export type { DynAvatarProps, AvatarSize } from '../types/avatar.types';
 export type { DynLabelProps } from '../types/label.types';
 export type { DynIconProps as DynIconPropsNew } from '../types/icon.types';
+
+// Utility types
+export type { ThemeSwitcherProps } from './ThemeSwitcher/ThemeSwitcher';
 
 // Constants
 export { DYN_COLOR_PALETTE } from '../types/badge.types';
