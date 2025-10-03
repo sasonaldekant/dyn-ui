4c97d5588e6355744ae4055a6de50ee54e4635ba
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-03 02:13:53 +0200

    Update components index to include all Display Components - SCOPE 5

--- packages/dyn-ui-react/src/components/index.ts
+++ packages/dyn-ui-react/src/components/index.ts
@@ -1,16 +1,30 @@
 /**
  * Main component exports for DYN UI React
- * Fixed exports to only include existing components
+ * Updated to include Display Components Group - SCOPE 5
  */
 
-// Basic components that exist
+// Basic components
 export { DynButton } from './DynButton';
 export { DynIcon } from './DynIcon';
 
+// Display Components - SCOPE 5
+export { default as DynBadge } from './DynBadge';
+export { default as DynAvatar } from './DynAvatar';
+export { default as DynLabel } from './DynLabel';
+
 // Layout components
 export { DynContainer } from './DynContainer';
-// Removed DynDivider - component doesn't exist yet
 
 // Export types
 export type { DynButtonProps } from './DynButton/DynButton.types';
 export type { DynIconProps } from './DynIcon/DynIcon';
+
+// Display Components types
+export type { DynBadgeProps, BadgeStatus, BadgeSize } from '../types/badge.types';
+export type { DynAvatarProps, AvatarSize } from '../types/avatar.types';
+export type { DynLabelProps } from '../types/label.types';
+export type { DynIconProps as DynIconPropsNew } from '../types/icon.types';
+
+// Constants
+export { DYN_COLOR_PALETTE } from '../types/badge.types';
+export { AVATAR_SIZES } from '../types/avatar.types';
