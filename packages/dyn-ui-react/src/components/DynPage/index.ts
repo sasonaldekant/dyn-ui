138cb02f4e9f5a4017c6629786a05ce712eb7d96
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 02:19:50 +0200

    Add DynPage index exports - Layout Components Group SCOPE 7

---
+++ packages/dyn-ui-react/src/components/DynPage/index.ts
@@ -0,0 +1,7 @@
+/**
+ * DynPage exports
+ * Part of DYN UI Layout Components Group - SCOPE 7
+ */
+
+export { DynPage } from './DynPage';
+export type { DynPageProps, DynPageBreadcrumb, DynPageAction } from '../../types/layout.types';
