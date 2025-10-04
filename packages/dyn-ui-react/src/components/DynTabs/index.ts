21f3a0f03b04f2eabfc5b38b2111f62107d87682
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 05:27:37 +0200

    Create DynTabs barrel export - clean API surface for tab navigation component

---
+++ packages/dyn-ui-react/src/components/DynTabs/index.ts
@@ -0,0 +1,12 @@
+/**
+ * DynTabs - Tab Navigation Component
+ * Barrel export for clean API surface
+ */
+
+export { default as DynTabs, DynTabs } from './DynTabs';
+export type { 
+  DynTabsProps, 
+  DynTabsHandle, 
+  TabItem 
+} from './DynTabs.types';
+export { TABS_DEFAULTS, TAB_POSITIONS, TAB_VARIANTS } from './DynTabs.types';
