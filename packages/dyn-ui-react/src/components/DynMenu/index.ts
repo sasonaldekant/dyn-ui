413ece659fd76f1d5624e08f65c351a73d075487
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 04:16:54 +0200

    Create DynMenu barrel export - clean API surface for navigation menu component

---
+++ packages/dyn-ui-react/src/components/DynMenu/index.ts
@@ -0,0 +1,14 @@
+/**
+ * DynMenu - Navigation Menu Component
+ * Barrel export for clean API surface
+ */
+
+export { default as DynMenu, DynMenu } from './DynMenu';
+export type { 
+  DynMenuProps, 
+  DynMenuRef, 
+  MenuItem, 
+  MenuBadge, 
+  MenuLiterals 
+} from './DynMenu.types';
+export { DEFAULT_MENU_LITERALS } from './DynMenu.types';
