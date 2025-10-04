4617373b679b2f87a13de937290950ebfc6006f3
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 02:35:57 +0200

    Update main index to export Layout Components - SCOPE 7

--- packages/dyn-ui-react/src/index.tsx
+++ packages/dyn-ui-react/src/index.tsx
@@ -15,6 +15,49 @@ export type {
 } from './components';
 export { DYN_COLOR_PALETTE, AVATAR_SIZES } from './components';
 
+// Form Components - SCOPE 6
+export {
+  DynInput,
+  DynSelect,
+  DynCheckbox,
+  DynDatePicker,
+  DynFieldContainer
+} from './components';
+export type {
+  DynInputProps,
+  DynSelectProps,
+  DynCheckboxProps,
+  DynDatePickerProps,
+  DynFieldContainerProps,
+  ValidationRule,
+  DynFieldRef,
+  DynFieldBase,
+  SelectOption
+} from './components';
+export { useDynFieldValidation, validators, useDynMask, MASK_PATTERNS, getMaskPattern, useDynDateParser, DATE_FORMATS, getDateFormat } from './components';
+
+// Layout Components - SCOPE 7
+export {
+  DynContainer,
+  DynDivider,
+  DynGrid,
+  DynPage
+} from './components';
+export type {
+  DynContainerProps,
+  DynDividerProps,
+  DynGridProps,
+  DynGridColumn,
+  DynPageProps,
+  DynPageBreadcrumb,
+  DynPageAction,
+  LayoutSize,
+  LayoutSpacing,
+  LayoutDirection,
+  LayoutAlignment,
+  LayoutJustify
+} from './components';
+
 // Theme System
 export { ThemeProvider, useTheme } from './theme/ThemeProvider';
 export type { ThemeProviderProps, ThemeContextValue, Theme } from './theme/ThemeProvider';
