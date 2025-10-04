bb83f73fdf1b6cee6807be3d3a3bd90cc6509352
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 04:21:09 +0200

    Create DynBreadcrumb barrel export - clean API surface for breadcrumb component

---
+++ packages/dyn-ui-react/src/components/DynBreadcrumb/index.ts
@@ -0,0 +1,12 @@
+/**
+ * DynBreadcrumb - Navigation Breadcrumb Component
+ * Barrel export for clean API surface
+ */
+
+export { default as DynBreadcrumb, DynBreadcrumb } from './DynBreadcrumb';
+export type { 
+  DynBreadcrumbProps, 
+  DynBreadcrumbRef, 
+  BreadcrumbItem 
+} from './DynBreadcrumb.types';
+export { DEFAULT_SEPARATOR, BREADCRUMB_LITERALS } from './DynBreadcrumb.types';
