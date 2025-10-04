b99e8b2830a8af717035c78566926334b34ee60b
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 02:18:26 +0200

    Add DynGrid index exports - Layout Components Group SCOPE 7

---
+++ packages/dyn-ui-react/src/components/DynGrid/index.ts
@@ -0,0 +1,7 @@
+/**
+ * DynGrid exports
+ * Part of DYN UI Layout Components Group - SCOPE 7
+ */
+
+export { DynGrid } from './DynGrid';
+export type { DynGridProps, DynGridColumn } from '../../types/layout.types';
