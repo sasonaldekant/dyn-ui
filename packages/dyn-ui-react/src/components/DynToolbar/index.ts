a97c56361d13f05e996cef638f8c73be41e910ba
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-04 04:51:14 +0200

    Create DynToolbar barrel export - clean API surface for toolbar component

---
+++ packages/dyn-ui-react/src/components/DynToolbar/index.ts
@@ -0,0 +1,12 @@
+/**
+ * DynToolbar - Responsive Toolbar Component
+ * Barrel export for clean API surface
+ */
+
+export { default as DynToolbar, DynToolbar } from './DynToolbar';
+export type { 
+  DynToolbarProps, 
+  DynToolbarRef, 
+  ToolbarItem 
+} from './DynToolbar.types';
+export { TOOLBAR_DEFAULTS, TOOLBAR_ITEM_TYPES } from './DynToolbar.types';
