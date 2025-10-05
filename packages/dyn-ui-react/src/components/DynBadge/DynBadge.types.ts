50d6ff336eb9fc4fafe8d2c78f8a87c3264058d0
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-05 18:46:13 +0200

    feat(DynBadge): add missing types file for local exports

---
+++ packages/dyn-ui-react/src/components/DynBadge/DynBadge.types.ts
@@ -0,0 +1,13 @@
+/**
+ * TypeScript interfaces for DynBadge component
+ * Re-exports from centralized badge types for consistency
+ */
+
+export type {
+  DynBadgeProps,
+  BadgeStatus,
+  BadgeSize,
+  BadgeIcon
+} from '../../types/badge.types';
+
+export { DYN_COLOR_PALETTE } from '../../types/badge.types';
