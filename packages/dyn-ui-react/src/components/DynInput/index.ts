f5e3f7fdfa605a0a73b1025480bf8e13edb7b4a4
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-05 02:01:23 +0200

    Standardize DynInput index exports

--- packages/dyn-ui-react/src/components/DynInput/index.ts
+++ packages/dyn-ui-react/src/components/DynInput/index.ts
@@ -1,2 +1,4 @@
+// Standardized exports for DynInput component
 export { DynInput } from './DynInput';
 export { default } from './DynInput';
+export type { DynInputProps } from '../../types/field.types';
