d85543a4f2e53b577c8fd8d453badebb0b9b4d33
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-05 19:45:43 +0200

    Update react-demo test setup to use vitest matchers

--- apps/react-demo/test-setup.ts
+++ apps/react-demo/test-setup.ts
@@ -1,1 +1,11 @@
-export * from '../../test-setup';
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+// extends Vitest's expect method with methods from React Testing Library
+expect.extend(matchers);
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
