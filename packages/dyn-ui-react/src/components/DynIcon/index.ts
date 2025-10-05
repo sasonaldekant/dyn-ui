bbf72b365638c54cc06bf11aaa601204bfd8efa6
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-05 17:35:06 +0200

    Fix DynIcon index.ts exports to include proper types

--- packages/dyn-ui-react/src/components/DynIcon/index.ts
+++ packages/dyn-ui-react/src/components/DynIcon/index.ts
@@ -1,5 +1,5 @@
 // Standardized exports for DynIcon component
-export { DynIcon } from './DynIcon';
-export { default } from './DynIcon';
+export { DynIcon, default } from './DynIcon';
+export type { DynIconProps, IconDictionary } from '../../types/icon.types';
 export type { IconDictionaryContext } from '../../providers/IconDictionaryProvider';
 export * from './icons';
