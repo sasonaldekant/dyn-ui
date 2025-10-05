da16d9e822588249547c437ea4ca520a6134ad15
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-05 02:02:51 +0200

    Standardize DynTable index exports

--- packages/dyn-ui-react/src/components/DynTable/index.ts
+++ packages/dyn-ui-react/src/components/DynTable/index.ts
@@ -1,3 +1,4 @@
+// Standardized exports for DynTable component
+export { DynTable } from './DynTable';
 export { default } from './DynTable';
-export type * from './DynTable.types';
-export { default as DynTable } from './DynTable';
+export type { DynTableProps, TableColumn, TableAction, TableData } from './DynTable.types';
