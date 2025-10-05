53fdd4914f79b646c484d1f11842996f2897aaf9
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-06 01:44:20 +0200

    Add missing component exports to fix test imports

--- packages/dyn-ui-react/src/index.ts
+++ packages/dyn-ui-react/src/index.ts
@@ -1,6 +1,65 @@
 // Entry point for dyn-ui-react components
 
+// Button Components
 export { default as DynButton } from './components/DynButton/DynButton';
+
+// Display Components
 export { default as DynIcon } from './components/DynIcon/DynIcon';
+export { DynAvatar } from './components/DynAvatar/DynAvatar';
+export { DynBadge } from './components/DynBadge/DynBadge';
+export { DynImage } from './components/DynImage/DynImage';
+export { DynTag } from './components/DynTag/DynTag';
+export { DynGauge } from './components/DynGauge/DynGauge';
+export { DynChart } from './components/DynChart/DynChart';
+export { DynKanban } from './components/DynKanban/DynKanban';
+
+// Layout Components
+export { DynPage } from './components/DynPage/DynPage';
+export { DynGrid } from './components/DynGrid/DynGrid';
+export { DynCard } from './components/DynCard/DynCard';
+export { DynContainer } from './components/DynContainer/DynContainer';
+export { DynCollapse } from './components/DynCollapse/DynCollapse';
+export { DynDivider } from './components/DynDivider/DynDivider';
+export { DynDrawer } from './components/DynDrawer/DynDrawer';
+export { DynSplit } from './components/DynSplit/DynSplit';
+export { DynStackLayout } from './components/DynStackLayout/DynStackLayout';
+
+// Navigation Components
+export { default as DynTabs } from './components/DynTabs/DynTabs';
+export { default as DynStepper } from './components/DynStepper/DynStepper';
+export { DynBreadcrumb } from './components/DynBreadcrumb/DynBreadcrumb';
+export { DynMenu } from './components/DynMenu/DynMenu';
+export { DynPagination } from './components/DynPagination/DynPagination';
+export { DynPopover } from './components/DynPopover/DynPopover';
+export { DynTooltip } from './components/DynTooltip/DynTooltip';
+
+// Form Components
+export { DynInput } from './components/DynInput/DynInput';
+export { DynSelect } from './components/DynSelect/DynSelect';
+export { DynCheckbox } from './components/DynCheckbox/DynCheckbox';
+export { DynRadio } from './components/DynRadio/DynRadio';
+export { DynSlider } from './components/DynSlider/DynSlider';
+export { DynSwitch } from './components/DynSwitch/DynSwitch';
+export { DynTextarea } from './components/DynTextarea/DynTextarea';
+export { DynDatepicker } from './components/DynDatepicker/DynDatepicker';
+export { DynUpload } from './components/DynUpload/DynUpload';
+
+// Feedback Components
+export { DynAlert } from './components/DynAlert/DynAlert';
+export { DynConfirmDialog } from './components/DynConfirmDialog/DynConfirmDialog';
+export { DynLoading } from './components/DynLoading/DynLoading';
+export { DynModal } from './components/DynModal/DynModal';
+export { DynToast } from './components/DynToast/DynToast';
+
+// Data Display Components
+export { DynTable } from './components/DynTable/DynTable';
+export { DynTree } from './components/DynTree/DynTree';
+export { DynTimeline } from './components/DynTimeline/DynTimeline';
+
+// Utility Components
+export { DynPortal } from './components/DynPortal/DynPortal';
+
+// Types
+export * from './types';
 
 // Add other components here as they are implemented
