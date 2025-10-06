4ce47d6fc14ba91b7ca6c4eb4a21ac500cd50037
m.gasic <m.gasic@LTC0IT244>
2025-10-06 02:52:00 +0200

    lable.tzpes.ts - add DynButtonProps interface

--- packages/dyn-ui-react/src/types/label.types.ts
+++ packages/dyn-ui-react/src/types/label.types.ts
@@ -3,16 +3,19 @@ import { BaseComponentProps } from './theme';
 export interface DynLabelProps extends BaseComponentProps {
   /** Associated form element ID */
   htmlFor?: string;
-  
+
   /** Disabled state */
   disabled?: boolean;
-  
+
   /** Required field indicator */
   required?: boolean;
-  
+
   /** Optional field indicator */
   optional?: boolean;
-  
+
   /** Help text to display */
   helpText?: string;
+
+  /** Additional HTML attributes */
+  [key: string]: any;
 }
