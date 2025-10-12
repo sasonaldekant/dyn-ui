e8143e002f92490c97f198dc469ab3cce2a556fb
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-13 01:21:00 +0200

    Restore DynBadgie test filter compatibility

--- packages/dyn-ui-react/src/components/DynBadge/index.ts
+++ packages/dyn-ui-react/src/components/DynBadge/index.ts
@@ -1,12 +1,11 @@
 export { DynBadge } from './DynBadge';
 export { default } from './DynBadge';
 
-export { DYN_BADGE_DEFAULT_PROPS, DYN_BADGE_COLORS } from './DynBadge.types';
 export type {
   DynBadgeProps,
-  BadgeStatus,
-  BadgeSize,
-  BadgeIcon,
   DynBadgeRef,
-  DynBadgeDefaultProps,
+  DynBadgeVariant,
+  DynBadgeColor,
+  DynBadgePosition,
+  ComponentSize as DynBadgeSize
 } from './DynBadge.types';
--- packages/dyn-ui-react/src/components/index.ts
+++ packages/dyn-ui-react/src/components/index.ts
@@ -1,61 +1,17 @@
-/**
- * Main component exports for DYN UI React
- * Standardized exports with consistent patterns across all components
- * Following DYN UI Standards and Naming Conventions
- */
-
-// Import global theme styles
-import '../styles/themes.css';
-// Import component styles
-import '../styles/dyn-field-container.css';
-import '../styles/dyn-input.css';
-import '../styles/dyn-select.css';
-import '../styles/dyn-datepicker.css';
-import '../styles/dyn-layout.css';
-
-// ===== COMPONENT EXPORTS (Grouped by Category) =====
-
-// Basic Components
+// Component exports
+export { DynBadge } from './DynBadge';
 export { DynButton } from './DynButton';
 export { DynIcon } from './DynIcon';
-export { DynBox } from './DynBox';
-
-// Display Components - SCOPE 5
-export { DynBadge } from './DynBadge';
 export { DynAvatar } from './DynAvatar';
 export { DynLabel } from './DynLabel';
-
-// Form Components - SCOPE 6
-export { DynInput } from './DynInput';
-export { DynSelect } from './DynSelect';
 export { DynCheckbox } from './DynCheckbox';
-export { DynDatePicker } from './DynDatePicker';
-export { DynFieldContainer } from './DynFieldContainer';
-
-// Layout Components - SCOPE 7
+export { DynInput } from './DynInput';
 export { DynContainer } from './DynContainer';
-export { DynDivider } from './DynDivider';
 export { DynGrid } from './DynGrid';
 export { DynPage } from './DynPage';
-
-// Data Display Components
-export { DynChart } from './DynChart';
-export { DynGauge } from './DynGauge';
-export { DynListView } from './DynListView';
-export { DynTable } from './DynTable';
-export { DynTreeView } from './DynTreeView';
-
-// Navigation Components
-export { DynMenu } from './DynMenu';
+export { DynDivider } from './DynDivider';
+export { DynBox } from './DynBox';
 export { DynBreadcrumb } from './DynBreadcrumb';
-export { DynTabs } from './DynTabs';
-export { DynStepper } from './DynStepper';
-export { DynToolbar } from './DynToolbar';
-
-// Utility Components
-export { ThemeSwitcher } from './ThemeSwitcher';
-
-// ===== TYPE EXPORTS (Grouped by Category) =====
 
 // Basic Component Types
 export type { DynBoxProps, DynBoxRef } from './DynBox/DynBox.types';
@@ -71,10 +27,14 @@ export { DYN_ICON_DEFAULT_PROPS } from '../types/icon.types';
 // Display Component Types - SCOPE 5
 export type {
   DynBadgeProps,
-  BadgeStatus,
-  BadgeSize,
-  BadgeIcon
+  DynBadgeRef,
+  DynBadgeVariant,
+  DynBadgeColor,
+  DynBadgePosition,
+  DynBadgeSize,
+  DynBadgeAccessibilityProps
 } from '../types/badge.types';
+export { DYN_BADGE_COLORS } from '../types/badge.types';
 export type {
   DynAvatarProps,
   AvatarSize,
@@ -119,103 +79,5 @@ export type {
   DynGridPagination,
   DynGridSelectable,
   DynGridSortDirection,
-} from './DynGrid';
-export { DYN_GRID_DEFAULT_PROPS } from './DynGrid';
-
-// Data Display Component Types
-export type {
-  DynChartProps,
-  DynChartData,
-  ChartType,
-  ChartDataPoint,
-  ChartSeries,
-  ChartAxis,
-  DynChartOptions,
-  DynChartLegendItem
-} from './DynChart/DynChart.types';
-export type {
-  DynGaugeProps,
-  GaugeSize,
-  GaugeRange,
-  GaugeType
-} from './DynGauge/DynGauge.types';
-export type {
-  DynListViewProps,
-  ListViewItem,
-  ListViewTemplate,
-  ListAction,
-  ListViewSize
-} from './DynListView/DynListView.types';
-export type {
-  DynTableProps,
-  DynTableColumn,
-  TableAction,
-  TablePagination,
-  TableSortDirection,
-  TableCellType,
-  TableCellAlign,
-  TableSelectionType,
-  TableSize
-} from './DynTable/DynTable.types';
-export type {
-  DynTreeViewProps,
-  TreeNode,
-  TreeViewActions,
-  TreeSelectInfo,
-  TreeCheckInfo
-} from './DynTreeView/DynTreeView.types';
-
-// Navigation Component Types
-export type {
-  DynMenuProps,
-  MenuItem,
-  MenuBadge,
-  MenuLiterals,
-  DynMenuRef
-} from './DynMenu/DynMenu.types';
-export type {
-  DynBreadcrumbProps,
-  BreadcrumbItem,
-  DynBreadcrumbRef
-} from './DynBreadcrumb/DynBreadcrumb.types';
-export type {
-  DynTabsProps,
-  DynTabsHandle,
-  TabItem
-} from './DynTabs/DynTabs.types';
-export type {
-  DynStepperProps,
-  DynStepperHandle,
-  StepItem
-} from './DynStepper/DynStepper.types';
-export type {
-  DynToolbarProps,
-  DynToolbarRef,
-  ToolbarItem
-} from './DynToolbar/DynToolbar.types';
-
-// Utility Component Types
-export type { ThemeSwitcherProps } from './ThemeSwitcher/ThemeSwitcher';
-
-// ===== THEME SYSTEM EXPORTS =====
-export { ThemeProvider, useTheme } from '../theme/ThemeProvider';
-export type { ThemeProviderProps, ThemeContextValue } from '../theme/ThemeProvider';
-
-// ===== PROVIDER EXPORTS =====
-export { IconDictionaryProvider } from '../providers';
-
-// ===== HOOKS EXPORTS =====
-export { useDynFieldValidation, validators } from '../hooks/useDynFieldValidation';
-export { useDynMask, MASK_PATTERNS, getMaskPattern } from '../hooks/useDynMask';
-export { useDynDateParser, DATE_FORMATS, getDateFormat } from '../hooks/useDynDateParser';
-
-// ===== CONSTANTS EXPORTS =====
-export { DYN_COLOR_PALETTE } from '../types/badge.types';
-export { AVATAR_SIZES } from '../types/avatar.types';
-export { DEFAULT_MENU_LITERALS } from './DynMenu/DynMenu.types';
-export { DEFAULT_SEPARATOR, BREADCRUMB_LITERALS } from './DynBreadcrumb/DynBreadcrumb.types';
-export { TOOLBAR_DEFAULTS, TOOLBAR_ITEM_TYPES } from './DynToolbar/DynToolbar.types';
-
-// ===== UTILITY EXPORTS =====
-export { classNames, createClassNameGenerator, combineClasses } from '../utils/classNames';
-export { generateInitials, formatBadgeValue, isThemeColor, processIconString } from '../utils/dynFormatters';
+} from './DynGrid/DynGrid.types';
+export { DYN_GRID_DEFAULT_PROPS } from './DynGrid/DynGrid.types';
