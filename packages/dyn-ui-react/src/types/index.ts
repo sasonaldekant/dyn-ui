2d7d40826309209b762ae964a22cfa100d3cfc5b
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-12 23:50:32 +0200

    Align DynBadge styles with available design tokens

--- packages/dyn-ui-react/src/types/index.ts
+++ packages/dyn-ui-react/src/types/index.ts
@@ -15,12 +15,19 @@ export type {
 export type { ThemeName, ThemeConfig, ThemeContextValue, ColorVariant, Size } from './theme';
 
 // Display Components types - SCOPE 5
-export type { DynBadgeProps, BadgeStatus, BadgeSize, BadgeIcon } from './badge.types';
+export type {
+  DynBadgeProps,
+  DynBadgeRef,
+  DynBadgeVariant,
+  DynBadgeColor,
+  DynBadgePosition,
+  DynBadgeSize,
+  DynBadgeAccessibilityProps
+} from './badge.types';
 export type { DynAvatarProps, AvatarSize, AvatarLoading } from './avatar.types';
 export type { DynIconProps, IconDictionary, ProcessedIcon } from './icon.types';
 export type { DynLabelProps } from './label.types';
 
-// Form Components types - SCOPE 6
 // Form Components types - SCOPE 6
 export type {
   DynInputProps,
@@ -67,5 +74,4 @@ export type {
 export { DYN_GRID_DEFAULT_PROPS } from '../components/DynGrid/DynGrid.types';
 
 // Constants
-export { DYN_COLOR_PALETTE } from './badge.types';
 export { AVATAR_SIZES } from './avatar.types';
