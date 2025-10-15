d5c8bf22eff438762f4ead55654bca66af34a619
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-15 23:35:44 +0200

    chore(DynTabs): unify index export (default+named+types) to satisfy component export pattern

--- packages/dyn-ui-react/src/components/DynTabs/index.ts
+++ packages/dyn-ui-react/src/components/DynTabs/index.ts
@@ -1,15 +1,3 @@
-/**
- * DynTabs Component Exports
- * Following DynAvatar export pattern for consistency
- */
-
-export { DynTabs } from './DynTabs';
-export type {
-  DynTabsProps,
-  DynTabsRef,
-  DynTabItem,
-  DynTabsSize,
-  DynTabsPosition,
-  DynTabsVariant
-} from './DynTabs.types';
-export { DYN_TABS_DEFAULT_PROPS } from './DynTabs.types';
+export { default as DynTabs } from "./DynTabs";
+export { DynTabs } from "./DynTabs";
+export type * from "./DynTabs.types";
