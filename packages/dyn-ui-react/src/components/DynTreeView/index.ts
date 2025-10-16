535128bec126893538f4bfd6bf04f0745f0199c8
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-16 02:54:24 +0200

    feat(DynTreeView): standardize import/export pattern and minimize public API

--- packages/dyn-ui-react/src/components/DynTreeView/index.ts
+++ packages/dyn-ui-react/src/components/DynTreeView/index.ts
@@ -1,5 +1,2 @@
-// Standardized exports for DynTreeView component
 export { DynTreeView } from './DynTreeView';
-export { default } from './DynTreeView';
-
-export type { DynTreeViewProps, TreeNode, TreeViewActions } from './DynTreeView.types';
+export type { DynTreeViewProps } from './DynTreeView.types';
