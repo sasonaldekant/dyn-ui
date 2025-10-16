9ffe3eac293b36dfa05a9f011d4c2fbc57f87163
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-16 02:48:47 +0200

    feat(DynGrid): standardize import/export pattern and minimize public API

--- packages/dyn-ui-react/src/components/DynGrid/index.ts
+++ packages/dyn-ui-react/src/components/DynGrid/index.ts
@@ -1,11 +1,2 @@
 export { DynGrid } from './DynGrid';
-export { default } from './DynGrid';
-
-export { DYN_GRID_DEFAULT_PROPS } from './DynGrid.types';
-export type {
-  DynGridProps,
-  DynGridColumn,
-  DynGridPagination,
-  DynGridSelectable,
-  DynGridSortDirection,
-} from './DynGrid.types';
+export type { DynGridProps } from './DynGrid.types';
