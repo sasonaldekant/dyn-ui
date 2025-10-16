3fdffff666be49f4da2dada2afcce2131931a2d6
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-16 02:26:42 +0200

    feat(DynSelect): standardize import/export pattern and export props type

--- packages/dyn-ui-react/src/components/DynSelect/index.ts
+++ packages/dyn-ui-react/src/components/DynSelect/index.ts
@@ -1,3 +1,2 @@
-// Standardized exports for DynSelect component
 export { DynSelect } from './DynSelect';
-export { default } from './DynSelect';
+export type { DynSelectProps } from './DynSelect.types';
