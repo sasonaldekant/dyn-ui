6c9b7be6d2d9954d1d4d075ce12fc60eb6b15677
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-16 02:18:33 +0200

    feat(DynContainer): standardize import/export pattern and minimize public API

--- packages/dyn-ui-react/src/components/DynContainer/index.ts
+++ packages/dyn-ui-react/src/components/DynContainer/index.ts
@@ -1,6 +1,2 @@
-// Standardized exports for DynContainer component
 export { DynContainer } from './DynContainer';
-export { default } from './DynContainer';
-
-export { DYN_CONTAINER_DEFAULT_PROPS } from './DynContainer.types';
-export type { DynContainerProps, DynContainerRef } from './DynContainer.types';
+export type { DynContainerProps } from './DynContainer.types';
