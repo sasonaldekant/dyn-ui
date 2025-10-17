cd5b3f093d984415bef385cd1db57df41a5db9f8
mgasic <48404494+mgasic@users.noreply.github.com>
2025-10-17 12:29:55 +0200

    feat(DynSelect): Update import to use CSS module instead of SCSS
    - Change import from DynSelect.module.scss to DynSelect.module.css
    - Fix Vite test resolution error
    - Complete SCSS to CSS migration for DynSelect component

--- packages/dyn-ui-react/src/components/DynSelect/DynSelect.tsx
+++ packages/dyn-ui-react/src/components/DynSelect/DynSelect.tsx
@@ -1,11 +1,11 @@
 import React, { forwardRef, useEffect, useMemo, useState } from 'react';
 import { cn } from '../../utils/classNames';
 import { generateId } from '../../utils/accessibility';
-import styles from './DynSelect.module.scss';
+import styles from './DynSelect.module.css';
 import type { DynSelectProps } from './DynSelect.types';
 
-// NOTE: file was previously importing './DynSelect.module.css' which does not exist.
-// This import fixes the Vite resolution error in tests.
+// NOTE: Import path updated from './DynSelect.module.scss' to './DynSelect.module.css'
+// This fixes the Vite resolution error in tests by using CSS module instead of SCSS.
 
 export const DynSelect = forwardRef<HTMLDivElement, DynSelectProps>(function DynSelect(props, ref) {
   // component code unchanged; this commit only fixes the stylesheet import path
