c6ec01e98906ed9765c10289d11da6dd2f1feef8
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-10-18 19:21:02 +0200

    Add DynTextArea component export

---
+++ packages/dyn-ui-react/src/components/DynTextArea/index.ts
@@ -0,0 +1,7 @@
+/**
+ * DynTextArea component exports
+ * Part of DYN UI Form Components Group
+ */
+
+export { DynTextArea as default, DynTextArea } from './DynTextArea';
+export type { DynTextAreaProps } from '../../types/field.types';
